011b2e632aece3cf4f439fe0b50b44fe4d72bc15
Lucas Vieceli <lucas.vieceli2@gmail.com>
2021-04-04 14:18:46 -0300

    Mudanças

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,8 @@
-console.log('Aula sobre function')
+//criar um aplicativo de frases motivacionais
+
+//declaration
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciencia e persistencia')
+    console.log('Revisao é a mae do aprendizado')
+}
